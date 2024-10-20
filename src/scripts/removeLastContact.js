460f6db5fab87731432f56b220a4ff59305db4b0
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-10-20 23:57:58 +0300

    1

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -3,10 +3,12 @@ import { writeContacts } from "../utils/writeContacts.js";
 
 export const removeLastContact = async () => {
   const contacts = await readContacts();
+
   if (contacts.length === 0) {
     console.log("There are no contacts to remove.");
     return;
   }
+
   contacts.pop();
   await writeContacts(contacts);
 };
