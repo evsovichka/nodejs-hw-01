b651e9d0aec5a76e9fb14257f5388b757d011edd
Yevsovych Kateryna <159715045+evsovichka@users.noreply.github.com>
2024-10-20 15:03:16 +0300

    ready

---
+++ src/scripts/generateContacts.js
@@ -0,0 +1,11 @@
+import { readContacts } from "../utils/readContacts.js";
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+const generateContacts = async (number) => {
+  const contacts = await readContacts();
+  const newContacts = Array(number).fill(0).map(createFakeContact);
+  await writeContacts([...contacts, ...newContacts]);
+};
+
+generateContacts(5);
---
+++ src/scripts/removeLastContact.js
@@ -0,0 +1,14 @@
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+  const contacts = await readContacts();
+  if (contacts.length === 0) {
+    console.log("There are no contacts to remove.");
+    return;
+  }
+  contacts.pop();
+  await writeContacts(contacts);
+};
+
+removeLastContact();
